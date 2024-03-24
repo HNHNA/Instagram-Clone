146724343a2ba828c1e319374989d3dea554d06b
Hanzdev <huynhngocanhittt@gmail.com>
2024-03-24 22:54:31 +0700

    Sidenav

---
+++ src/timeline/Timeline.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Timeline.css"
+import Suggestions from './Suggestions'
+
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+      <div className='timeline_left'>
+        TimeLine
+      </div>
+      <div className='timeline_right'>
+        <Suggestions />
+      </div>
+    </div>
+  )
+}
+
+export default Timeline
